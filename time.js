30a49271936e317b1d7ad36d50e33f31062cd341
leeyunhome <leeyunhome@daum.net>
2020-03-27 09:33:28 +0900

    modify time.js, index.html

--- time.js
+++ time.js
@@ -1,12 +1,20 @@
-const h1 = document.querySelector(".time");
+// const h1 = document.querySelector(".time");
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
 
-function display() {
-    h1.innerTEXT = "hello";
-    console.log(h1);
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours }:${minutes}:${seconds < 10 ? `0${seconds}` : seconds }`;
 }
 
+
 function init() {
-    display();
+    getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
